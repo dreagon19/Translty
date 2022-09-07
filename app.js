5886eb9249df6cc09e619f0034ca2da878514edc
Jitesh Kumar <60359722+dreagon19@users.noreply.github.com>
2022-09-07 14:35:25 +0530

    Cleaning up code and adding comments for Juniors

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 $(function () {
   $(".ShowThis").hide();
   let furl = "";
+  //File Picker 
   document.querySelector("#formFileLg").addEventListener("change", function () {
     let url = URL.createObjectURL(this.files[0]);
     furl = url;
-    //console.log(url);
   });
 
+  //The big blue button that says "Read Aloud". This Functions runs when that button is clicked
   $("#SpeakBtn").click(function () {
     $(".ShowThis").show();
 
     let textFinal = "";
     Tesseract.recognize(furl, "eng", {
       logger: (m) => {
-        let width = Math.ceil(m["progress"] * 100);
-        //console.log(width);
+        let width = Math.ceil(m["progress"] * 100);    // This gives the percentage in the progress bar
         $(".progress-bar").text(`${width}%`).css("width", `${width}%`);
-        
       },
     }).then(({ data: { text } }) => {
       textFinal = text;
-      textFinal = textFinal.replace(/\n/g, " ");
+
+      // Replaces all the new line in the extracted text to spaces,(Makes it easier for the model to read)
+      textFinal = textFinal.replace(/\n/g, " "); 
 
       Speak(textFinal);
     });
@@ -28,14 +29,17 @@ $(function () {
   function Speak(text) {
     let textArr = text.split(" ");
     textArr.unshift(" ");
-    console.log(textArr);
 
     $("#inputDiv").hide("slow");
 
     let sp = new SpeechSynthesisUtterance();
     sp.text = text;
-    window.speechSynthesis.speak(sp);
+    sp.rate = 1; //TODO: Higher Rate means speaking fast
+    
+    window.speechSynthesis.speak(sp); //Models starts speaking here
 
+
+    //Below lines shows the text on browser , that are being spoken right now
     let i = 0;
     sp.onboundary = (event) => {
       if (textArr[i] != undefined) {
